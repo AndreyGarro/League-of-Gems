0720c1b4638f326a8ec36de9075526d343f48b5c
Andrey Sibaja <diego.garro120@gmail.com>
2018-10-06 14:36:37 -0600

    Commit incial, se agrega la libreria Allegro, sale pantalla en negro

---
+++ cmake-build-debug/CMakeFiles/LeagueOfGems.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LeagueOfGems.dir/main.cpp.o"
+  "LeagueOfGems.pdb"
+  "LeagueOfGems"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LeagueOfGems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <allegro5/allegro.h>
+
+ALLEGRO_DISPLAY *display;
+
+int main(int argc, char **argv) {
+
+    al_init();
+
+    display = al_create_display(640, 480);
+    al_clear_to_color(al_map_rgb_f(0,0,0));
+    al_flip_display();
+    al_rest(5.0);
+    al_destroy_display(display);
+    return 0;
+}
+
+void init(){
+    int depth, res;
+//    depth = desktop_color_depth();
+}
